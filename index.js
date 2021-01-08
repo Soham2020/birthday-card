3bf18a32855f2a7ad62e2e0419e5c40be0a50efd
Soham Das <majumder70.ranu@gmail.com>
2021-01-08 20:21:39 +0530

    js work added

---
+++ index.js
@@ -0,0 +1,28 @@
+const generatePDF = async(name) => {
+    const { PDFDocument, rgb } = PDFLib;
+
+    const exBytes = await fetch("./bdaycert.pdf").then((res) => {
+        return res.arrayBuffer()
+    });
+    // console.log(exBytes);
+    const exFonts = await fetch("Sanchez-Regular.ttf").then((res) => {
+        return res.arrayBuffer()
+    });
+
+    const pdfDoc = await PDFDocument.load(exBytes)
+    pdfDoc.registerFontkit(fontkit);
+    const myFont = await pdfDoc.embedFont(exFonts);
+
+    const pages = pdfDoc.getPages();
+    const firstPg = pages[0];
+    firstPg.drawText(name, {
+        x: 310,
+        y: 300,
+        size: 54,
+        color: rgb(1,0.6,1)
+    })
+    const uri = await pdfDoc.saveAsBase64({dataUri: true})
+    window.open(uri)
+    document.querySelector("#mypdf").src = uri;
+}
+generatePDF("Soham Das")
